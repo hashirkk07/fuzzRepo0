757f87fb035d134150816565d26b4d9ec4948c78
hashirkk07 <kkhashir07@gmail.com>
2021-11-14 07:34:04 +0530

    first

---
+++ clike/A/B/cFuzz.cpp
@@ -0,0 +1,10 @@
+#include <stdint.h>
+#include <stddef.h>
+
+bool FuzzMe(const uint8_t *Data, size_t DataSize) {
+  return DataSize >= 3 &&
+      Data[0] == 'F' &&
+      Data[1] == 'U' &&
+      Data[2] == 'Z' &&
+      Data[3] == 'Z';
+}
