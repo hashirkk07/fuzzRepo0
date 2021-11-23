fac7be284f1fed5694b1c02b97cd0a593948f1f3
Hashir K K <62003821+hashirkk07@users.noreply.github.com>
2021-11-23 10:22:39 +0530

    Update pyFuzzCode.py

--- python/A/P/pyFuzzCode.py
+++ python/A/P/pyFuzzCode.py
@@ -2,4 +2,4 @@ def pyParseComplex(data):
 	if len(data) == 5 :
 		if data[0] == 'F' and data[1] == 'U' and data[2] == 'Z' and data[3] == 'Z' and data[4] == 'I' and data[5] == 'T':
 			return True
-	return false
+	return False
