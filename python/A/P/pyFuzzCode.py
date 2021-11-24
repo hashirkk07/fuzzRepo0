95661ff1f11d605f4d6828e35ee373439616f4db
Hashir K K <62003821+hashirkk07@users.noreply.github.com>
2021-11-24 14:49:25 +0530

    Update pyFuzzCode.py

--- python/A/P/pyFuzzCode.py
+++ python/A/P/pyFuzzCode.py
@@ -1,5 +1,3 @@
-def pyParseComplex(data):
-	if len(data) == 5 :
-		if data[0] == 'F' and data[1] == 'U' and data[2] == 'Z' and data[3] == 'Z' and data[4] == 'I' and data[5] == 'T':
-			return True
-	return False
+def BadCatcher(data):
+	if data == b"OH YOU FUZZED ME!":
+		raise RuntimeError("Entered Restricted Space")
